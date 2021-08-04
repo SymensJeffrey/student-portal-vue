0b540121cf46014e829a661a6dc22b3bab37413a
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-04 18:43:32 -0500

    fix nav bar and remove extra login

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/ViewResume">Show Resume</router-link> 
+      <router-link to="/ViewResume">Show Resume</router-link> |
       <router-link to="/Login">Login</router-link>
-      <router-link to="/login">Login</router-link>
 
     </div>
     <router-view/>
