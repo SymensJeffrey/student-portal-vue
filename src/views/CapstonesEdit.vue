76080905f4e4cbae62a0b7a0ede2fd11dd29aa94
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-03 20:00:04 -0500

    add input boxes and and update capstone function

--- src/views/CapstonesEdit.vue
+++ src/views/CapstonesEdit.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="capstone-edit">
+  <div class="home">
     <h1>{{ message }}</h1>
+    
+    <p> Name: <input type="text" /></p>
+    <p> Description: <input type="text" /></p>
+    <p> URL: <input type="text" /></p>
+    <p> Screenshot: <input type="file" /></p>
+    <p> <button v-on:click="updateCapstone(capstone)"> Update </button> </p>
   </div>
+
 </template>
 
 <style></style>
 
 <script>
+  // import axios from "axios"
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
+        message: "Edit Capstone",
+        capstone: {}
       };
     },
     created: function () {},
-    methods: {},
+    methods: {
+      updateCapstone: function(capstone) {
+        console.log("updating...")
+        var editCapstoneParams = capstone;
+        axios.patch("/capstones" + editCapstoneParams.id).then((response) => {
+          console.log("Capstone Update", response);
+          // this.$router.push("/capstones");
+        })
+        .catch((error) => {
+          console.log("capstones update error", error.response);
+          this.errors = error.response.data.errors;
+        });
+      },
+    },
   };
 </script>
