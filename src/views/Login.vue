81d41664b582018e9ed01ff0c06758feb1996f4a
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-08-04 20:37:15 -0500

    add login info into a card

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,21 +17,24 @@
         <input type="password" v-model="newSessionParams.password" />
       </div> -->
 
-
-        <form v-on:submit.prevent="test()">
-          <h1>Login</h1>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="newSessionParams.email">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" v-model="newSessionParams.password">
-          </div>
-        
-          <input type="submit" value="Login" />
-        </form>
+      <div class="card">
+        <div class="card-body">
+          <form v-on:submit.prevent="test()">
+            <h1>Login</h1>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="newSessionParams.email">
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" v-model="newSessionParams.password">
+            </div>
+          
+            <input type="submit" value="Login" />
+          </form>
+        </div>
+      </div>  
     <!-- </form> -->
 
   </div>
