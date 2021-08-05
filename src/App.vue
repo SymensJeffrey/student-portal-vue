13582413b65e07ba55b16f31268199fe7be76ca7
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-04 20:03:55 -0500

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/Login">Login</router-link> |
       <router-link to="/ViewResume">Show Resume</router-link> |
-      <router-link to="/capstones/:id">Show Capstone</router-link> 
+      <router-link to="/capstones/:id">Show Capstone</router-link>  -->
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Student Portal</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/Login">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/ViewResume">View Resume</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/capstones/:id">View Capstone</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
