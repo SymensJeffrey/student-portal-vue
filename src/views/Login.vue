1d28090d7bddd25fa1616c9a45839c1c0f264e0b
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-08-04 20:54:06 -0500

    add padding to show background

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="login">
+    <br />
+    <br />
+
     <!-- <form v-on:submit.prevent="test()">
       <h1>Login</h1>
       <ul> -->
@@ -16,31 +19,48 @@
         <label>Password:</label>
         <input type="password" v-model="newSessionParams.password" />
       </div> -->
-
-      <div class="card">
-        <div class="card-body">
-          <form v-on:submit.prevent="test()">
-            <h1>Login</h1>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="newSessionParams.email">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1" v-model="newSessionParams.password">
-            </div>
-          
-            <input type="submit" value="Login" />
-          </form>
-        </div>
-      </div>  
     <!-- </form> -->
 
+    <div class="card">
+      <div class="card-body">
+        <form v-on:submit.prevent="test()">
+          <h1>Login</h1>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="newSessionParams.email">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" v-model="newSessionParams.password">
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    </div>  
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
   </div>
-  
-
-  
 </template>
 
 <script>
